e8a80acdef53c6b70e1db868943b642f7d580b94
[dyad] <yashmm77@gmail.com>
2026-03-13 14:00:09 +0530

    Reverted all changes back to version b23f289c58ecf9af5516225b731a7f3497668ff6

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import NotFound from "./pages/NotFound";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Team from "./pages/Team";
-import Dashboard from "./pages/Dashboard";
 
 const App = () => (
   <Router>
@@ -14,7 +13,6 @@ const App = () => (
       <Route path="/signup" element={<Signup />} />
       <Route path="/" element={<Layout><Index /></Layout>} />
       <Route path="/team" element={<Layout><Team /></Layout>} />
-      <Route path="/dashboard" element={<Layout><Dashboard /></Layout>} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   </Router>
