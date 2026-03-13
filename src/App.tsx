591ca526e4f34cba0b3bf59039e11baa27c64795
[dyad] <yashmm77@gmail.com>
2026-03-13 13:37:56 +0530

    [dyad] Implemented the Dashboard page with summary cards, recent tasks, and tasks due today using Supabase views and color-coded badges. - wrote 5 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import NotFound from "./pages/NotFound";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Team from "./pages/Team";
+import Dashboard from "./pages/Dashboard";
 
 const App = () => (
   <Router>
@@ -13,6 +14,7 @@ const App = () => (
       <Route path="/signup" element={<Signup />} />
       <Route path="/" element={<Layout><Index /></Layout>} />
       <Route path="/team" element={<Layout><Team /></Layout>} />
+      <Route path="/dashboard" element={<Layout><Dashboard /></Layout>} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   </Router>
